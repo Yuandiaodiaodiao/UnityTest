13da18a0d9fcf4325f59315d614973d14f260c94
im-cy <44048665+im-cy@users.noreply.github.com>
2018-10-14 02:01:04 +0800

    faq

--- ut/Unity Test/Assets/Assets_/camera/cameraLink.cs
+++ ut/Unity Test/Assets/Assets_/camera/cameraLink.cs
@@ -1,25 +1,45 @@
 ﻿using System.Collections;
+
 using System.Collections.Generic;
+
 using UnityEngine;
 
+
+
 public class cameraLink : MonoBehaviour
+
 {
+
     public Transform followTarget;
+
     public float cameraDist;
-    private float rotateDegree;
+
+
+
     // Use this for initialization
+
     void Start()
+
     {
-        rotateDegree = 0;
+
+
 
     }
+
+
 
     // Update is called once per frame
+
     void Update()
+
     {
 
-        rotateDegree += 0.01f;
-        transform.position = followTarget.position + cameraDist  *(Quaternion.AngleAxis(-30, followTarget.right) * followTarget.forward);
+
+
+        transform.position = followTarget.position + cameraDist * (Quaternion.AngleAxis(-30, followTarget.right) * followTarget.forward);
+
         transform.rotation = Quaternion.LookRotation(followTarget.position - transform.position);
+
     }
+
 }
